8176b85d5f4369d972e0595fd7ab6072225f660c
schedrov <rocolusso@icloud.com>
2022-04-01 16:04:04 +0300

    complete

---
+++ js/main.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// // 1
+//
+// let x = 10, y = 7;
+// (x>y) ? console.log('x больше, чем y') : console.log('x не больше, чем y');
+
+
+// //2
+
+// const num = prompt('Введите число');
+// if ( num % 2 === 0){
+//     alert ( "Число " + num + " четное" );
+// }else{
+//     alert ( "Число " + num + " нечетное" );
+// }
+
+
+// // 3
+
+
+// let a = prompt('Введите целое число ');
+//
+// if (a>0){
+//
+//     if (a.length===1){
+//         console.log(`Число ${a} однозначное положительное`)
+//     }
+//     if (a.length===2){
+//         console.log(`Число ${a} двузначное положительное`)
+//     }
+//     if (a.length===3){
+//         console.log(`Число ${a} трехзначное положительное`)
+//     }
+//     if (a.length===4){
+//         console.log(`Число ${a} четырехзначное положительное`)
+//     }
+//
+// } else {
+//
+//     if (a.length===2){
+//         console.log(`Число ${a} однозначное отрицательное`)
+//     }
+//     if (a.length===3){
+//         console.log(`Число ${a} двузначное отрицательное`)
+//     }
+//     if (a.length===4){
+//         console.log(`Число ${a} трехзначное отрицательное`)
+//     }
+//
+//     if (a.length===5){
+//         console.log(`Число ${a} четырехзначное отрицательное`)
+//     }
+// }
+
+
+//// 4
+
+// const firstNum = +prompt('Введите первое число');
+// const secondNum = +prompt('Введите второе число');
+// const thirdNum = +prompt('Введите третье число');
+//
+// const maxNum=Math.max(firstNum,secondNum,thirdNum);
+//
+// if (firstNum === maxNum) {
+//    alert(`Наибольшее число ${firstNum}`);
+// } else if (secondNum === maxNum) {
+//     alert(`Наибольшее число ${secondNum}`);
+// } else   {
+//     alert(`Наибольшее число ${thirdNum}`);
+// }
+
+
+//// 5
+
+// const a = +prompt('Введите сторону a');
+// const b = +prompt('Введите сторону b');
+// const c = +prompt('Введите сторону c');
+//
+//
+// if (a>0 && b>0 && c>0 ) {
+//
+//     if ( (a+b) >c && (c+b) >a && (a+c) >b ) {
+//         alert('Стороны треугольника введены правильно');
+//     }
+//
+// }else {
+//     alert('Треугольник невозможен');
+// }
+
